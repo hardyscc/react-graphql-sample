f64444dcdc6f80d2b8a99368fd18bc4825d53729
Hardys <hardyscc@gmail.com>
2020-03-01 20:25:58 +0800

    add duplicate check sample

--- src/components/AddUser.tsx
+++ src/components/AddUser.tsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { UsersStore } from "../context/UsersStore";
 
 export const AddUser = () => {
-  const { addUser } = UsersStore.useContainer();
-
   const [name, setName] = useState("");
+  const { users, addUser } = UsersStore.useContainer();
+
   return (
     <div>
       <input value={name} onChange={e => setName(e.target.value)} />
       <button
         onClick={e => {
+          if (users.filter(user => user.name === name).length > 0) {
+            alert("already exists");
+            return;
+          }
           addUser({ name });
           setName("");
         }}
