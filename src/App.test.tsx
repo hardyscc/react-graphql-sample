2506f81211c062b7d7b0447fb94027f08fdf3234
Hardys <hardyscc@gmail.com>
2020-06-25 12:10:06 +0800

    fix: test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { MockedProvider } from "@apollo/react-testing";
+import { render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import { UsersDocument } from "./generated/graphql";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mocks = [
+  {
+    request: {
+      query: UsersDocument,
+    },
+    result: {
+      data: {
+        users: [{ __typename: "User", id: "1", name: "John", nickName: null }],
+      },
+    },
+  },
+];
+
+test("renders UserList", async () => {
+  const { findByText } = render(
+    <MockedProvider mocks={mocks}>
+      <App />
+    </MockedProvider>
+  );
+
+  expect(await findByText("John")).toBeInTheDocument();
 });
